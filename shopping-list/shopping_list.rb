29872ac8c807d727c30be7e66e4c4f7a5527e8bf
Jacob Roman <jcbrmn06@gmail.com>
2019-10-10 22:23:35 -0400

    chore: inital commit

---
+++ shopping-list/shopping_list.rb
@@ -0,0 +1,21 @@
+def create_list
+  print "What is the list name? "
+  name = gets.chomp
+
+  hash = { "name" => name, "items" => Array.new}
+  return hash
+end
+
+def add_list_item
+  print "What is the item called? "
+  item_name = gets.chomp
+
+  print "How much> "
+  quantity = gets.chomp.to_i
+
+  hash = { "name" => item_name, "quantity" => quantity}
+end
+
+list = create_list()
+puts list.inspect
+list['items'].push(add_list_item())
